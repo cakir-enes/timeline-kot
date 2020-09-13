4be263c1862530e9d7dd279f4e081b6b9423d07f
ecakir <ecakir@reddlyne.com>
2020-09-13 23:58:21 +0300

    fxc

--- src/main/kotlin/com/example/demo/AppConfiguration.kt
+++ src/main/kotlin/com/example/demo/AppConfiguration.kt
@@ -2,8 +2,15 @@ package com.example.demo
 
 import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.CqlSessionBuilder
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.connection.RedisSocketConfiguration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import java.net.InetSocketAddress
 
 @Configuration
@@ -17,4 +24,9 @@ class AppConfiguration {
                 .build()
         return sess
     }
+
+//    @Bean("reactive")
+//    fun redisConn(): ReactiveRedisConnectionFactory {
+//        return LettuceConnectionFactory(RedisSocketConfiguration("/var/run/redis.sock"))
+//    }
 }
--- src/main/kotlin/com/example/demo/FeedService.kt
+++ src/main/kotlin/com/example/demo/FeedService.kt
@@ -1,23 +1,31 @@
 package com.example.demo
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.supervisorScope
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.domain.Range
+import org.springframework.data.redis.connection.RedisZSetCommands
+import org.springframework.data.redis.core.*
 import org.springframework.stereotype.Service
+import reactor.util.function.Tuple2
+import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalUnit
 import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.Comparator
+import kotlin.math.max
 
 @Service
-class FeedService(private val repo: Repository) {
+class FeedService(private val repo: Repository, private val redisTemplate: ReactiveRedisTemplate<String, String>) {
 
-    private val dateFormatter = ThreadLocal.withInitial { DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone( ZoneId.systemDefault() ) }
+    private val dateFormatter = ThreadLocal.withInitial { DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault()) }
+    private val mapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+    private val zset: ReactiveZSetOperations<String, String> = redisTemplate.opsForZSet()
 
     suspend fun getFeedOfUser(userId: Long, limit: Int, before: Instant): List<FeedPost> = supervisorScope {
 
@@ -32,8 +40,16 @@ class FeedService(private val repo: Repository) {
             val s: SortedSet<PostInfo> = sortedSetOf(Comparator { o1, o2 -> if (o1.postedAt.isAfter(o2.postedAt)) 1 else -1 })
             s.addAll(feedItems.filter { !user.blockedUsers.contains(it.authorId) })
             subbedCategoryItems.collect { categItems ->
-                s.addAll(categItems.filter { !user.blockedUsers.contains(it.authorId) })
+                val infos = categItems
+                s.addAll(infos.filter { !user.blockedUsers.contains(it.authorId) })
             }
+//            subbedCategoryItems.collect { (categId, categItems) ->
+//                val infos = if (categItems.isEmpty())
+//                    getAndCacheCategoryPosts(categId, before, limit)
+//                else
+//                    categItems
+//                s.addAll(infos.filter { !user.blockedUsers.contains(it.authorId) })
+//            }
             s.take(20)
         }()
 
@@ -89,6 +105,26 @@ class FeedService(private val repo: Repository) {
                         .map { it.await() }
                         .forEach { emit(it) }
             }
+
+    private suspend fun getAndCacheCategoryPosts(categoryId: Int, before: Instant, limit: Int): List<PostInfo> {
+        val infos = repo.getTopCategoryPosts(categoryId, before, limit)
+        val ser = infos.map { DefaultTypedTuple(mapper.writeValueAsString(it), it.postedAt.toEpochMilli().toDouble()) }
+        zset.addAll("category:$categoryId", ser).subscribe()
+        redisTemplate.expire("category:$categoryId", Duration.ofMinutes(1)).subscribe()
+        return infos
+    }
+
+    private suspend fun getCategoryPostsFromCache(categories: Collection<Int>, before: Instant, limit: Int): Flow<Pair<Int, List<PostInfo>>> =
+            categories
+                    .asFlow()
+                    .flatMapMerge(concurrency = max(categories.size, 1)) { id ->
+                        val range = Range.closed(Double.MIN_VALUE, before.toEpochMilli().toDouble())
+                        flow {
+                            emit(id to zset.reverseRangeByScoreAsFlow("category:$id", range, RedisZSetCommands.Limit.limit().count(limit))
+                                    .map { mapper.readValue(it, PostInfo::class.java) }
+                                    .toList())
+                        }
+                    }
 
     private suspend fun getPosts(ids: List<Long>): Map<Long, Post> = coroutineScope {
         ids.map { async { repo.getPost(it) } }
